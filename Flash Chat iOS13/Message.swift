2060fd6f25735431f8bbb13165902687b862bde3
CoelhoNunes <dcoelho12@hotmail.com>
2022-03-25 22:19:37 -0400

    Frist Commit

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Daniel Coelho on 8/17/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String // Email Address of sender
+    let body: String
+}
