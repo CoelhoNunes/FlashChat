88051399d058afe75822caad77cbb4e79ea82d63
CoelhoNunes <dcoelho12@hotmail.com>
2022-03-25 22:25:14 -0400

    First Commit

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -1,11 +1,3 @@
-//
-//  WelcomeViewController.swift
-//  Flash Chat iOS13
-//
-//  Created by Angela Yu on 21/10/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
-
 import UIKit
 import CLTypingLabel
 
@@ -28,7 +20,7 @@ class WelcomeViewController: UIViewController {
         
         titleLabel.text = K.appName 
         
-//          BRO JUST DO IT THIS WAY! FUCK THE WHOLE TERMIAL BULL SHIT!
+//          THIS WAY!
 //        titleLabel.text = ""
 //        var charIndex = 0.0
 //        let titleText = "⚡️FlashChat"
--- Flash Chat iOS13/Message.swift
+++ Flash Chat iOS13/Message.swift
@@ -1,11 +1,3 @@
-//
-//  Message.swift
-//  Flash Chat iOS13
-//
-//  Created by Daniel Coelho on 8/17/21.
-//  Copyright © 2021 Angela Yu. All rights reserved.
-//
-
 import Foundation
 
 struct Message {
